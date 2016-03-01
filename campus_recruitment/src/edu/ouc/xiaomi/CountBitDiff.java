64e16130b3bcc0403d46c186acdbf66a983711db
TopGunViper <1449978622@qq.com>
2016-03-01 23:00:18 +0800

    xiaomi

---
+++ campus_recruitment/src/edu/ouc/xiaomi/CountBitDiff.java
@@ -0,0 +1,14 @@
+package edu.ouc.xiaomi;
+
+public class CountBitDiff {
+    /**
+     * 获得两个整形二进制表达位数不同的数量
+     * 
+     * @param m 整数m
+     * @param n 整数n
+     * @return 整型
+     */
+    public int countBitDiff(int m, int n) {
+    	return 0;
+    }
+}
