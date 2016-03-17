e57c075c6aa8d587fa4f381cebe4cc9a2d5b4214
TopGunViper <1449978622@qq.com>
2016-03-17 23:32:01 +0800

    数值的整数次方

---
+++ src/edu/ouc/offer/Power.java
@@ -0,0 +1,15 @@
+package edu.ouc.offer;
+
+/**
+ * 
+题目描述
+给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+
+ * @author wqx
+ *
+ */
+public class Power {
+    public double Power(double base, int exponent) {
+        return Math.pow(base, exponent);
+  }
+}
