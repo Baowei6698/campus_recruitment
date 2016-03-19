5d714aa6c8ad990668517263c2935eeb2687e1d3
TopGunViper <1449978622@qq.com>
2016-03-20 00:19:55 +0800

    全排列

--- src/edu/ouc/offer/PermutationDemo.java
+++ src/edu/ouc/offer/PermutationDemo.java
@@ -19,28 +19,59 @@ import java.util.TreeSet;
  *
  */
 public class PermutationDemo {
-    public ArrayList<String> Permutation(String str) {
-    	if(str == null || "".equals(str)) return new ArrayList<String>(0);
-        Set<String> result = new TreeSet<String>();
-        nextPermutation(result,str.toCharArray(),0);
-        return new ArrayList<String>(result);
-    }
-    public void nextPermutation(Set<String> result,char[] str,int cur){
-    	if(cur == str.length){
-    		result.add(new String(str));
-    		return ;
-    	}
-    	for(int i = cur; i < str.length; i++){
-    		swap(str,i,cur);
-    		nextPermutation(result,str,cur+1);
-    		swap(str,i,cur);	
-    	}
-    }
-    public void swap(char[]str,int i,int j){
-        if (i != j) {
-            char t = str[i];
-            str[i] = str[j];
-            str[j] = t;
-        }
-    }
+	public ArrayList<String> Permutation(String str) {
+		if(str == null || "".equals(str)) return new ArrayList<String>(0);
+		Set<String> result = new TreeSet<String>();
+		nextPermutation(result,str.toCharArray(),0);
+		return new ArrayList<String>(result);
+	}
+	public void nextPermutation(Set<String> result,char[] str,int cur){
+		if(cur == str.length){
+			result.add(new String(str));
+			return ;
+		}
+		for(int i = cur; i < str.length; i++){
+			swap(str,i,cur);
+			nextPermutation(result,str,cur+1);
+			swap(str,i,cur);	
+		}
+	}
+	public void swap(char[]str,int i,int j){
+		if (i != j) {
+			char t = str[i];
+			str[i] = str[j];
+			str[j] = t;
+		}
+	}
+	/**
+	 * step1：从右往左找到第一个破坏升序(非严格)的元素，此例中为4.记下标为 i
+
+       step2: 依然从右往左,找到第一个大于4的元素，此例中5，交换4和5.
+	
+       step3：从i+1到最右端，逆置。6 4 3 2 to 2 3 4 6
+	 * @param num
+	 */
+	void nextPermutation(int[]num) {		 
+		if (num.length == 0) return;		 
+		int i = num.length-1;		
+		int j;		
+		while(i>0 && num[i] <= num[i - 1]){
+			i--;
+		}
+		if (i != 0){
+			j = num.length - 1;
+			while (num[j]<= num[i-1])	
+				j--;					
+			swap(num,i-1,j);		
+		}//if
+		j = num.length - 1;		
+		while (i< j){			
+			swap(num,i,j);	
+			i++; j--;		 
+		}	
+	}	
+	void swap(int[] num,int a, int b){		
+		int c;
+		c = num[a]; num[a] = num[b]; num[b] = c; 
+	}
 }
