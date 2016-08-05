35ac4864243e200123b33b498b3a793176cad30c
TopGunViper <1449978622@qq.com>
2016-08-05 20:41:27 +0800

    misc：有向图Node

---
+++ src/main/java/edu/ouc/algorithm/graph/UndirectedGraphNode.java
@@ -0,0 +1,15 @@
+package edu.ouc.algorithm.graph;
+
+import java.util.ArrayList;
+
+public class UndirectedGraphNode {
+	
+    int label = 0;
+    UndirectedGraphNode left = null;
+    UndirectedGraphNode right = null;
+    ArrayList<UndirectedGraphNode> neighbors = new ArrayList<UndirectedGraphNode>();
+
+    public UndirectedGraphNode(int label) {
+        this.label = label;
+    }
+}
