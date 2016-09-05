64453a940642d3c0c61282d3370d07e589014876
TopGunViper <1449978622@qq.com>
2016-09-05 09:03:55 +0800

    LCS，不连续

--- src/main/java/edu/ouc/algorithm/dp/LCS.java
+++ src/main/java/edu/ouc/algorithm/dp/LCS.java
@@ -47,10 +47,47 @@ public class LCS {
 		}
 		System.out.println("maxLen:" + maxLen + ",res:" + sb.toString());
 	}
+	
+	/**
+	 * 最长公共子序列
+	 * 
+	 * @param s1
+	 * @param s2
+	 */
+	public void LCSII(String s1,String s2){
+		int n = s1.length(), m = s2.length();
+		int[][] dp = new int[n+1][m+1];
+		int maxLen = 0;
+		
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= m; j ++){
+				if(s1.charAt(i-1) == s2.charAt(j-1)){
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}else{
+					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+				}
+			}
+		}
+		System.out.println("maxLen:" + dp[s1.length()][s2.length()]);
+		//输出LCS
+		StringBuilder sb = new StringBuilder(0);
+		int pos1 = s1.length(), pos2 = s2.length();
+		while(pos1 > 0 && pos2 > 0){
+			if(s1.charAt(pos1-1) == s2.charAt(pos2-1)){
+				sb.append(s1.charAt(pos1-1));
+				pos2--;pos1--;
+			}else if(dp[pos1][pos2-1] >= dp[pos1-1][pos2]){
+				pos2--;
+			}else{
+				pos1--;
+			}
+		}
+		System.out.println("lcs:" + sb.reverse().toString());
+	}
 	public static void main(String[] args) {
-		String s1 = "babac";
-		String s2 = "cabadfd";
-		new LCS().LCSI(s1, s2);
+		String s1 = "babdcac";
+		String s2 = "cabffdfda";
+		new LCS().LCSII(s1, s2);
 	}
 
 }
